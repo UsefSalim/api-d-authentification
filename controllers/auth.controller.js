748612545375bfcc9e16fde46fdab4bf53875502
ucef8233 <-u>
2021-01-11 00:49:21 +0100

    refactorisation du code pour la verification du mail

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,28 +1,31 @@
 const User = require('../models/user.model');
+const bcrypt = require('bcryptjs')
 const { registerValidation } = require('../validations/auth.validation')
 
-exports.register = async (req, res) => {
-  const { email, password, name } = req.body
+exports.register = async (req, res, next) => {
+  // const { email, password, name } = req.body
   //? -------------------------------------------------------------  validate Data 
   const { error } = registerValidation(req.body)
   if (error) return res.status(400).send(error.details[0].message)
   //? ------------------------------------------------------------verify if mail exist 
-  // try {
-  const emailExist = await User.findOne({ email: email })
-  if (emailExist) return res.status(400).send(`l'adress mail : ${req.body.email}  , existe deja`)
-  // } catch (error) {
-  //   res.status(400).send({ error })
-  // }
-  //* -------------------------------------------------------------  create a new user
-  const user = new User({
-    ...req.body
-  })
-  //!--------------------------------------------------------  register the user in the database
   try {
-    await user.save()
-    res.status(201).send({ user: user._id, message: "Utulisateur crée avec succée" })
+    const ifExist = await User.findOne({ email: req.body.email }).exec()
+    if (ifExist) return res.status(400).send(`l'adress mail : ${req.body.email}  , existe deja`)
+    //* -------------------------------------------------------------  create a new user
+    const user = new User({
+      email: req.body.email,
+      name: req.body.name,
+      password: req.body.password
+    })
+    //!--------------------------------------------------------  register the user in the database
+    try {
+      await user.save()
+      res.status(201).send({ user: user._id, message: "Utulisateur crée avec succée" })
+    } catch (error) {
+      res.status(400).send({ error })
+    }
   } catch (error) {
-    res.status(400).send({ error })
+    res.status(500).send({ error })
   }
 }
 
